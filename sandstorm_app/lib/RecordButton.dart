05bfeacdafe3476bc3ccc7af0cd5ccfe4e75d0fb
ophir <pere.jobs@gmail.com>
2018-09-05 23:49:24 +0200

    Implement file upload

--- sandstorm_app/lib/RecordButton.dart
+++ sandstorm_app/lib/RecordButton.dart
@@ -1,9 +1,15 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:http/http.dart';
+import 'package:http_parser/http_parser.dart';
 import 'package:simple_permissions/simple_permissions.dart';
+
+const UPLOAD_ENDPOINT = "http://192.168.42.43:5000/upload";
 
 class RecordButton extends StatefulWidget {
   RecordButton({Key key}) : super(key: key);
@@ -14,6 +20,7 @@ class RecordButton extends StatefulWidget {
 
 class _RecordButtonState extends State<RecordButton> {
   bool _isRecording = false;
+  String _filePath;
   FlutterSound flutterSound;
 
   @override
@@ -24,6 +31,11 @@ class _RecordButtonState extends State<RecordButton> {
   }
 
   void _startRecording() async {
+    if (_isRecording) {
+      // Avoid starting a new recording when one is started already
+      return;
+    }
+
     var permitted =
         await SimplePermissions.checkPermission(Permission.RecordAudio) &&
             await SimplePermissions.checkPermission(
@@ -36,24 +48,61 @@ class _RecordButtonState extends State<RecordButton> {
       return;
     }
 
-    var now = DateTime.now().toIso8601String();
     try {
-      var res = await flutterSound.startRecorder(null);
-      _showError(res);
+      var filepath = await flutterSound.startRecorder(null);
+      _showError(filepath);
       setState(() {
+        _filePath = filepath;
         _isRecording = true;
       });
     } on Exception catch (e) {
       _showError("Unable to start recording: $e");
+      _setRecordingState(false);
     }
   }
 
-  void _stopRecording() async {
-    var res = await flutterSound.stopRecorder();
-    _showError("$res");
+  void _setRecordingState(bool isRecording) {
     setState(() {
-      _isRecording = false;
+      _isRecording = isRecording;
     });
+  }
+
+  void _stopRecording() async {
+    await flutterSound.stopRecorder();
+    _setRecordingState(false);
+
+    var recordingFile = new File(_filePath);
+
+    try {
+      _showSuccess("Sending the recording...");
+      await _uploadFile(recordingFile);
+      _showSuccess("The recording was sent.");
+    } catch (e) {
+      _showError("Unable to send the recording: $e");
+    }
+  }
+
+  _uploadFile(File file) async {
+    var bytesStream = file.openRead();
+    var fileSize = file.statSync().size;
+
+    var request = new MultipartRequest("POST", Uri.parse(UPLOAD_ENDPOINT));
+    var mediaType = new MediaType("audio", "mp4");
+    var multipartFile = MultipartFile("recording", bytesStream, fileSize,
+        filename: "recording.m4a", contentType: mediaType);
+    request.files.add(multipartFile);
+
+    var response = await request.send();
+
+    if (response.statusCode != 200)
+      throw "Server error: ${response.reasonPhrase}";
+
+    var responseObject = await response.stream
+        .transform(utf8.decoder)
+        .transform(json.decoder)
+        .first;
+
+    return responseObject;
   }
 
   @override
@@ -82,9 +131,17 @@ class _RecordButtonState extends State<RecordButton> {
   }
 
   void _showError(String message) {
+    _showMessage(message, Colors.red);
+  }
+
+  void _showSuccess(String message) {
+    _showMessage(message, Colors.green);
+  }
+
+  void _showMessage(String message, Color color) {
     Scaffold.of(context).showSnackBar(new SnackBar(
       content: new Text(message),
-      backgroundColor: Colors.red,
+      backgroundColor: color,
     ));
   }
 }
